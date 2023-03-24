b5000b2e7c09f7c94ddcd0b8d25742caf3d48929
thiiagoms <thiagom.devsec@gmail.com>
2023-03-24 11:44:04 -0300

    feat(app): finish drum application :sparkles:

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+function playSound(soundElement) {
+    document.querySelector(soundElement).play();
+}
+
+const audioList = document.querySelectorAll('.tecla');
+
+audioList.forEach((soundButton) => {
+    
+    soundButton.onclick = function() {
+        soundButton.classList.add('ativa');
+        playSound(`#som_${soundButton.classList[1]}`);
+        soundButton.classList.remove('ativa');
+    };
+
+    soundButton.onkeydown = function(event) {
+        if (event.code == 'Space' || event.code == 'Enter') {
+            soundButton.classList.add('ativa');
+        }
+    }
+
+    soundButton.onkeyup = function () {
+        soundButton.classList.remove('ativa');
+    }
+})
